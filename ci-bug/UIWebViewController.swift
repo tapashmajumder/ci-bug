41cb5ad7aea002f33d25f012f3593c346cc8abb4
Tapash Majumder <tapash@iterable.com>
2019-11-27 10:46:41 +0530

    Change background color

--- ci-bug/UIWebViewController.swift
+++ ci-bug/UIWebViewController.swift
@@ -20,12 +20,20 @@ class UIWebViewController: UIViewController {
         
         let html = "<html><body style='text-align: center; font-size: 30px;'>Hello, World!<body></html><meta name='viewport' content='initial-scale=1.0' />"
 
-        view.backgroundColor = UIColor.clear
+        if #available(iOS 13, *) {
+            view.backgroundColor = UIColor.systemBackground
+        } else {
+            view.backgroundColor = UIColor.clear
+        }
         let webView = UIWebView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
         webView.loadHTMLString(html, baseURL: URL(string: ""))
         webView.scrollView.bounces = false
         webView.isOpaque = false
-        webView.backgroundColor = UIColor.clear
+        if #available(iOS 13, *) {
+            webView.backgroundColor = UIColor.systemBackground
+        } else {
+            webView.backgroundColor = UIColor.white
+        }
         
         view.addSubview(webView)
     }
--- ci-bug/WKWebViewController.swift
+++ ci-bug/WKWebViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 
-class WebViewController: UIViewController {
+class WKWebViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +21,21 @@ class WebViewController: UIViewController {
         
         let html = "<html><body style='text-align: center; font-size: 30px;'>Hello, World!<body></html><meta name='viewport' content='initial-scale=1.0' />"
         
-        view.backgroundColor = UIColor.clear
+        if #available(iOS 13, *) {
+            view.backgroundColor = UIColor.systemBackground
+        } else {
+            view.backgroundColor = UIColor.clear
+        }
         let webView = WKWebView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
         webView.loadHTMLString(html, baseURL: URL(string: ""))
         webView.scrollView.bounces = false
         webView.isOpaque = false
-        webView.backgroundColor = UIColor.clear
-        
+        if #available(iOS 13, *) {
+            webView.backgroundColor = UIColor.systemBackground
+        } else {
+            webView.backgroundColor = UIColor.white
+        }
+
         view.addSubview(webView)
     }
 
